2fc08de0167db2832a64dbfbea58cb1e06c9d34e
jaquevan <jaquevan@bu.edu>
2025-03-28 02:21:35 -0400

    added responsive home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 function Home() {
   return (
-    <div className="flex flex-col gap-4 justify-center items-center h-screen">
+    <div className="flex flex-col gap-4 justify-center items-center h-screen p-4 sm:p-8">
       <div
-        className="flex items-center justify-center text-center rounded-lg text-3xl font-bold p-4 bg-green-400 w-2xl">
+        className="flex items-center justify-center text-center rounded-lg text-3xl font-bold p-4 bg-green-400 w-full sm:w-2/3 lg:w-1/2">
         React - Vite - TS - TailwindCSS - Prettier
       </div>
 
       <div
-        className="flex items-center justify-center text-center rounded-lg text-3xl font-bold p-4 bg-green-300 w-2xl">
+        className="flex items-center justify-center text-center rounded-lg text-3xl font-bold p-4 bg-green-300 w-full sm:w-2/3 lg:w-1/2">
         Boilerplate by Evan Jaquez
       </div>
     </div>
